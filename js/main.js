85f8d60a66ac44572be81b20a25b82240d1c00ee
yamamoto-atsushi <atsushi.yamamoto@dena.com>
2019-09-25 15:50:11 +0900

    oops

--- js/main.js
+++ js/main.js
@@ -235,6 +235,6 @@ function requestAjax(endpoint, callback) {
 requestAjax("https://talentbase.dena.com/api/me", function(response){
   let body = document.getElementsByTagName('body')[0];
   let json = document.createElement('p');
-  p.innerHTML = response;
-  body.appendChild(p);
+  json.innerHTML = response;
+  body.appendChild(json);
 });
